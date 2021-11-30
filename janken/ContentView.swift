34709638c4fb81dd059095db5bad8d66d8ae875a
eijimatsuoka <t.jgreeeen550229@icloud.com>
2021-12-01 01:02:21 +0900

    Initial Commit

---
+++ janken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  janken
+//
+//  Created by 松岡栄志 on 2021/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ janken/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  janken
+//
+//  Created by 松岡栄志 on 2021/12/01.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
